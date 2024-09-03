0aa29b785b77e076d292e7c7153b569fe654b41e
Monisha <monishas3699@gmail.com>
2024-09-03 22:24:58 +0530

    Api call for User Details

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
-// src/App.js
-import React from "react";
-import BasicTable from "../src/componets/basic-table";
-import data from "./data.json";
+import React, { useState, useEffect } from 'react';
 
-function App() {
+function UserList() {
+  const [users, setUsers] = useState([]);  
+  const [loading, setLoading] = useState(true);  
+  const [error, setError] = useState(null);  
+
+  useEffect(() => {
+    fetch('https://randomuser.me/api/?results=5')  
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Unable to load the page!'); 
+        }
+        return response.json();  
+      })
+      .then((data) => {
+        setUsers(data.results);  
+        setLoading(false); 
+      })
+      .catch((error) => {
+        setError(error); 
+        setLoading(false);  
+      });
+  }, []); 
+
+  if (loading) return <p>Loading...</p>;  
+  if (error) return <p>Error: {error.message}</p>; 
+
   return (
     <div>
-      <h1>Expandable Table</h1>
-      <BasicTable data={data} />
+      <h1>UserS Details</h1>
+      <ul>
+        {users.map((user, index) => (
+          <li key={index}>
+            <h2>{user.name.first} {user.name.last}</h2>
+            <img src={user.picture.thumbnail} alt={`${user.name.first} ${user.name.last}`} />
+            <p>Email: {user.email}</p>
+            <p>Location: {user.location.city}, {user.location.country}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
 
-export default App;
+export default UserList;
