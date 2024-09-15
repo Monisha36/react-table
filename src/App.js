d53433a0c1761f073aeedc00ca40dc06ebc67730
Monisha <monishas3699@gmail.com>
2024-09-15 16:13:45 +0530

    stateManagement task1

--- src/App.js
+++ src/App.js
@@ -1,46 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import { useState } from "react";
 
-function UserList() {
-  const [users, setUsers] = useState([]);  
-  const [loading, setLoading] = useState(true);  
-  const [error, setError] = useState(null);  
+export default function App(){
+  const [count, setCount] = useState(0);
 
-  useEffect(() => {
-    fetch('https://randomuser.me/api/?results=5')  
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Unable to load the page!'); 
-        }
-        return response.json();  
-      })
-      .then((data) => {
-        setUsers(data.results);  
-        setLoading(false); 
-      })
-      .catch((error) => {
-        setError(error); 
-        setLoading(false);  
-      });
-  }, []); 
+  function handleClick(){
+    setCount(count+1);
+  }
 
-  if (loading) return <p>Loading...</p>;  
-  if (error) return <p>Error: {error.message}</p>; 
-
-  return (
-    <div>
-      <h1>UserS Details</h1>
-      <ul>
-        {users.map((user, index) => (
-          <li key={index}>
-            <h2>{user.name.first} {user.name.last}</h2>
-            <img src={user.picture.thumbnail} alt={`${user.name.first} ${user.name.last}`} />
-            <p>Email: {user.email}</p>
-            <p>Location: {user.location.city}, {user.location.country}</p>
-          </li>
-        ))}
-      </ul>
-    </div>
+  return(
+<div>
+  <h1>Counting the Clicks</h1>
+  <MyButton count={count} onClick={handleClick}/>
+</div>
   );
 }
 
-export default UserList;
+function MyButton({count, onClick}){
+  return(
+  <button onClick={onClick}>Clicked {count} times</button>
+);
+}
